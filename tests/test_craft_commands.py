f51efd3222177fdc36ba29d20ca473dda6ad21b9
Joseph Mancuso <idmann509@gmail.com>
2018-01-12 16:48:38 -0500

    changed assetion statements

--- tests/test_craft_commands.py
+++ tests/test_craft_commands.py
@@ -6,30 +6,30 @@ def test_craft_install():
     subprocess.call(['touch', '.env-example'])
     subprocess.call(['craft', 'install'])
     
-    assert os.path.isfile('.env')
+    assert os.path.exists('.env')
     
     subprocess.call(['rm', '.env'])
     subprocess.call(['rm', '.env-example'])
 
 def test_craft_new():
     subprocess.call(['craft', 'new', 'testnew'])
-    assert os.path.isdir('testnew')
+    assert os.path.exists('testnew')
 
 def test_craft_view():
     command = subprocess.Popen('cd testnew ; craft view test',stdout=subprocess.PIPE, shell=True)
     subprocess.Popen.wait(command)
-    assert os.path.isfile('testnew/resources/templates/test.html')
+    assert os.path.exists('testnew/resources/templates/test.html')
     subprocess.call(['rm', 'testnew/resources/templates/test.html'])
 
 def test_craft_controller():
     command = subprocess.Popen('cd testnew ; craft controller TestController',stdout=subprocess.PIPE, shell=True)
     subprocess.Popen.wait(command)
-    assert os.path.isfile('testnew/app/http/controllers/TestController.py')
+    assert os.path.exists('testnew/app/http/controllers/TestController.py')
     subprocess.call(['rm', 'testnew/app/http/controllers/TestController.py'])
 
 def test_craft_model():
     command = subprocess.Popen('cd testnew ; craft model TestModel',stdout=subprocess.PIPE, shell=True)
     subprocess.Popen.wait(command)
-    assert os.path.isfile('testnew/app/TestModel.py')
+    assert os.path.exists('testnew/app/TestModel.py')
     subprocess.call(['rm', 'testnew/app/TestModel.py'])
 
